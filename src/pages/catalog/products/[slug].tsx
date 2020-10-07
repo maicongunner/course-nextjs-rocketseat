ad2757f31e8801d6f7c2e24bc01df55bb924aeeb
Maicon Domingues <maikonbdomingues@gmail.com>
2020-10-07 07:30:54 -0300

    impr: loading product details from API

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,12 +1,20 @@
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import { useState } from "react";
+import { Document } from "prismic-javascript/types/documents";
+import PrismicDOM from "prismic-dom";
+import { client } from "@/lib/prismic";
+import { GetStaticPaths, GetStaticProps } from "next";
+
+interface ProductProps {
+  product: Document;
+}
 
 const AddToCartModal = dynamic(() => import("@/components/AddToCartModal"), {
   loading: () => <p>loading...</p>,
 });
 
-export default function Products() {
+export default function Products({ product }: ProductProps) {
   const router = useRouter();
   const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
 
@@ -14,12 +22,48 @@ export default function Products() {
     setIsAddToCartModalVisible(true);
   }
 
+  if (router.isFallback) {
+    return <p>loading...</p>;
+  }
+
   return (
     <>
-      <h1>{router.query.slug}</h1>
+      <h1>{PrismicDOM.RichText.asText(product.data.title)}</h1>
       <button onClick={handleAddToCart}>Add to cart</button>
-
       {isAddToCartModalVisible && <AddToCartModal />}
+
+      <div
+        dangerouslySetInnerHTML={{
+          __html: PrismicDOM.RichText.asHtml(product.data.description),
+        }}
+      ></div>
+
+      <p>Price: ${product.data.price}</p>
+
+      <img
+        src={product.data.thumbnail.url}
+        alt={product.data.title}
+        width="200"
+      />
     </>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
+  const { slug } = context.params;
+  const product = await client().getByUID("product", String(slug), {});
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 10,
+  };
+};
